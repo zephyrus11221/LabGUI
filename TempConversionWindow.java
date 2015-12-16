cd2927199f0e33a73b48d0904ef5f30adc9432f0
Stephan Doan <sdoan@stuy.edu>
2015-12-16 13:19:41 -0500

    Add Fahrenheit conversion

--- TempConversionWindow.java
+++ TempConversionWindow.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 public class TempConversionWindow extends JFrame implements ActionListener{
     private Container pane;
     private JLabel j;
-    private JTextField t;
+    private JTextField f;
 
     public TempConversionWindow() {
 	this.setTitle("Temperature Conversion");
@@ -17,33 +17,44 @@ public class TempConversionWindow extends JFrame implements ActionListener{
 	pane = this.getContentPane();
 	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 	
-	JButton b = new JButton("ByteMe");
+	JButton b = new JButton("To Fahrenheit");
 	b.addActionListener(this);
-	b.setActionCommand("Byte");
-	JButton b2 = new JButton("No...");
+	b.setActionCommand("toF");
+	JButton b2 = new JButton("To Celsius");
 	b2.addActionListener(this);
-	b2.setActionCommand("NotByte");
+	b2.setActionCommand("toC");
 	
 	
-	t = new JTextField(10);
+	f = new JTextField(10);
 	
-	JCheckBox c = new JCheckBox("OverClock");
 	j = new JLabel("Meaning of life is... ");
-	pane.add(c);
-	pane.add(t);
+	pane.add(f);
 	pane.add(b);
 	pane.add(b2);
 	pane.add(j);
     }
+
+    public static double CtoF(double t){
+	double otpt = t;
+	otpt= 9*otpt/5;
+	return otpt+32;
+    }
+    public static double FtoC(double t){
+	double otpt = t;
+	otpt -= 32;
+	otpt= 5*otpt/9;
+	return otpt;
+    }
     
     public void actionPerformed(ActionEvent e){
 	String event = e.getActionCommand();
-	if(event.equals("Byte")){
-	    String s = t.getText();
-	    s += "-0101000";
+	if(event.equals("toF")){
+	    double otpt = Double.parseDouble(f.getText());
+	    otpt = CtoF(otpt);
+	    String s = ""+otpt;
 	    j.setText(s);
 	}
-	if(event.equals("NotByte")){
+	if(event.equals("toC")){
 	    j.setText("Fish");
 	    
 	}
